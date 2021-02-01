587b32da87811448a2fe4c3a6b3bcf1231f74f86
Ruben Cardenas <rubencg88@gmail.com>
2021-02-01 17:08:51 -0600

    Added main placeholder for website. Closes #17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,16 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Select, Store } from '@ngxs/store';
-import { AccountActions, IncomeState, IncomeActions, ExpenseActions, ExpenseState, AccountState, CategoryActions } from './state';
-import { Income } from './income';
-import { Expense } from './expense';
-import { Account } from './account';
-import { TransferActions } from './state/transfer/transfer.actions';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <ul>
-      <li *ngFor='let item of items$ | async'>
-        {{item.description}}
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: 'app.component.html',
   styles: []
 })
 export class AppComponent {
   title = 'budget-manager-web';
-  @Select(AccountState.selectAccounts) items$: Observable<Account[]>;
-
+  
   constructor(private store: Store){
-    this.store.dispatch(new IncomeActions.Get());
-    this.store.dispatch(new AccountActions.Get());
-    this.store.dispatch(new ExpenseActions.Get());
-    this.store.dispatch(new CategoryActions.GetIncomeCategories());
-    this.store.dispatch(new CategoryActions.GetExpenseCategories());
-    this.store.dispatch(new TransferActions.Get());
     
   }
 }
