16e146b9529115f370ed06ba9e517b93d8131ff3
Ruben Cardenas <rubencg88@gmail.com>
2021-04-20 17:01:45 -0500

    Added incomes by category report

--- src/app/components/reports/pie-report/incomes-by-category/incomes-by-category.component.ts
+++ src/app/components/reports/pie-report/incomes-by-category/incomes-by-category.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+import { Income } from 'src/app/income';
 import { PieElement } from 'src/app/models';
+import { PieChartComponent } from '../pie-chart/pie-chart.component';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'incomes-by-category',
@@ -8,25 +14,51 @@ import { PieElement } from 'src/app/models';
 })
 export class IncomesByCategoryComponent implements OnInit {
 
-  data: PieElement[] = [
-    {
-      description: 'Salario',
-      color: '#32a852',
-      amount: 333.33,
-      icon: 'tshirt',
-      percentage: 97.09
-    },{
-      description: 'Bonos',
-      color: '#f5e042',
-      amount: 133.33,
-      icon: 'home',
-      percentage: 2.91
-    }
-  ];
+  @ViewChild(PieChartComponent) pieChart: PieChartComponent;
+  incomes$: Observable<PieElement[]>;
+  
+  constructor(private store: Store) { 
 
-  constructor() { }
+  }  
+  ngAfterViewInit(): void {
+    this.changeDate(new Date());
+  }
 
   ngOnInit(): void {
+  }
+
+  changeDate(date: Date){
+    this.incomes$ = this.store.select((state) => state.incomeState.incomes).pipe(
+      delay(0),
+      map((incomes: Income[]) => {
+        
+        // group incomes by category
+        const appliedincomes: Income[] = incomes.filter(
+          (t: Income) =>
+          t.date.getMonth() == date.getMonth() &&
+          t.date.getFullYear() == date.getFullYear() &&
+          t.isApplied
+          );
+        const incomesByCategory = _.groupBy(appliedincomes, (e: Income) => e.category.name);
+        const totalAmount = appliedincomes.reduce((a, b: Income) => +a + b.amount, 0);
+        
+        const pieElements: PieElement[] = _.map(incomesByCategory, function(value: Income[], key) {
+          const categoryAmount = value.reduce((a, b: Income) => +a + b.amount, 0);
+          const pieElement: PieElement = { 
+            description: key, 
+            amount: categoryAmount,
+            color: value[0].category.color,
+            icon: value[0].category.image,
+            percentage: +(categoryAmount / totalAmount * 100)
+          };
+          return pieElement;
+        });
+        
+        return pieElements.sort((a, b) => b.amount - a.amount );
+      })
+    );
+
+    this.pieChart.setData(this.incomes$);
   }
 
 }
--- src/app/components/reports/pie-report/pie-report.component.ts
+++ src/app/components/reports/pie-report/pie-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ExpensesByAccountComponent } from './expenses-by-account/expenses-by-account.component';
 import { ExpensesByCategoryComponent } from './expenses-by-category/expenses-by-category.component';
+import { IncomesByCategoryComponent } from './incomes-by-category/incomes-by-category.component';
 
 @Component({
   selector: 'pie-report',
@@ -10,6 +11,7 @@ import { ExpensesByCategoryComponent } from './expenses-by-category/expenses-by-
 export class PieReportComponent implements OnInit {
   @ViewChild(ExpensesByCategoryComponent) expensesByCategory: ExpensesByCategoryComponent;
   @ViewChild(ExpensesByAccountComponent) expensesByAccount: ExpensesByAccountComponent;
+  @ViewChild(IncomesByCategoryComponent) incomesByCategory: IncomesByCategoryComponent;
 
   constructor() { }
 
@@ -22,6 +24,9 @@ export class PieReportComponent implements OnInit {
     }
     if(this.expensesByAccount){
       this.expensesByAccount.changeDate(date);
+    }
+    if(this.incomesByCategory){
+      this.incomesByCategory.changeDate(date);
     }
   }
 
