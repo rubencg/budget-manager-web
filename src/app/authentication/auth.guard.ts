a850c29ec933dae12ec641df3d22d24f40cd9aa3
Ruben Cardenas <rubencg88@gmail.com>
2021-04-22 17:29:29 -0500

    Fixed login route issue

--- src/app/authentication/auth.guard.ts
+++ src/app/authentication/auth.guard.ts
@@ -1,14 +1,18 @@
 import { Injectable } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
 import { Store } from "@ngxs/store";
 import { AuthenticationState } from "../state/authentication/authentication.state";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private store: Store) {}
+  constructor(private store: Store, private router: Router) {}
 
   canActivate() {
     const isAuthenticated = this.store.selectSnapshot(AuthenticationState.isAuthenticated);
+
+    if(!isAuthenticated){
+      this.router.navigate(['login']);
+    }
     
     return isAuthenticated;
   }
