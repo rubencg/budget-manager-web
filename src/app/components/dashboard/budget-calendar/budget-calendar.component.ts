11254558ef977b3fce649b3a6d91ae5cfe89f581
Ruben Cardenas <rubencg88@gmail.com>
2021-04-26 14:13:11 -0500

    Added colored dots to budget calendar on main page

--- src/app/components/dashboard/budget-calendar/budget-calendar.component.ts
+++ src/app/components/dashboard/budget-calendar/budget-calendar.component.ts
@@ -1,6 +1,12 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
-
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { Transaction, TransactionTypes } from 'src/app/models';
+import { ExpenseState, IncomeState, TransferState } from 'src/app/state';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'budget-calendar',
@@ -9,9 +15,82 @@ import { CalendarEvent, CalendarView } from 'angular-calendar';
 })
 export class BudgetCalendarComponent implements OnInit {
   @Output() onDateChanged: EventEmitter<Date> = new EventEmitter();
-  constructor() {}
 
-  ngOnInit(): void {}
+  transfers$: Observable<Transaction[]>;
+  incomes$: Observable<Transaction[]>;
+  expenses$: Observable<Transaction[]>;
+  monthlyIncomes$: Observable<Transaction[]>;
+  monthlyExpenses$: Observable<Transaction[]>;
+  
+  constructor(public store: Store,
+    private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadCalendarEvents(this.viewDate);
+  }
+
+  loadCalendarEvents(date: Date){
+    this.transfers$ = this.store.select(
+      TransferState.selectTransactionsForMonth(date)
+    );
+    this.incomes$ = this.store.select(
+      IncomeState.selectTransactionsForMonth(date)
+    );
+    this.expenses$ = this.store.select(
+      ExpenseState.selectTransactionsForMonth(date)
+    );
+    this.monthlyIncomes$ = this.store.select(
+      IncomeState.selectMonthlyIncomeTransactionsForMonth(date)
+    );
+    this.monthlyExpenses$ = this.store.select(
+      ExpenseState.selectMonthlyExpenseTransactionsForMonth(date)
+    );
+    this.transfers$.subscribe((transfers: Transaction[]) => {
+      this.incomes$.subscribe((incomes: Transaction[]) => {
+        this.expenses$.subscribe((expenses: Transaction[]) => {
+          this.monthlyExpenses$.subscribe((monthlyExpenses: Transaction[]) => {
+            this.monthlyIncomes$
+              .pipe(delay(0))
+              .subscribe((monthlyIncomes: Transaction[]) => {
+                let source = transfers
+                        .concat(incomes)
+                        .concat(monthlyExpenses)
+                        .concat(monthlyIncomes)
+                        .concat(expenses);
+                this.events = [];
+
+                source.forEach(t => {
+                  let event = this.events.find(e => e.start.getDate() == t.date.getDate() 
+                    && e.cssClass == this.getCssClassByType(t.type));
+                  if(!event){
+                    this.events.push({
+                      title: '',
+                      start: t.date,
+                      cssClass: this.getCssClassByType(t.type)
+                    });
+                  }
+                });
+              });
+            });
+          });
+        });
+      });
+  }
+
+  getCssClassByType(type: TransactionTypes){
+    switch (type) {
+      case TransactionTypes.Income:
+        return 'day day-with-incomes';
+      case TransactionTypes.Expense:
+        return 'day day-with-expenses';
+      case TransactionTypes.Transfer:
+        return 'day day-with-transfers';
+      case TransactionTypes.MonthlyIncome:
+        return 'day day-with-unpaid-incomes';
+      case TransactionTypes.MonthlyExpense:
+        return 'day day-with-unpaid-expenses';
+    }
+  }
 
   view: CalendarView = CalendarView.Month;
 
@@ -20,10 +99,16 @@ export class BudgetCalendarComponent implements OnInit {
   events: CalendarEvent[] = [];
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
-    console.log('logged', date);
+    this.router.navigate(['/transactions'], {
+      queryParams: {
+        startDate: date.toISOString(),
+        endDate: date.toISOString(),
+      },
+    });
   }
 
   dateChanged(){
     this.onDateChanged.emit(this.viewDate);    
+    this.loadCalendarEvents(this.viewDate);
   }
 }
