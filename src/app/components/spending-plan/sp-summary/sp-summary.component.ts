977ca57f1d4f9ffb82a6c58d849f20e62209d577
Ruben Cardenas <rubencg88@gmail.com>
2025-01-03 21:25:53 -0500

    Added expense total to sp summary

--- src/app/components/spending-plan/sp-summary/sp-summary.component.ts
+++ src/app/components/spending-plan/sp-summary/sp-summary.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Transaction } from 'src/app/models';
-import { IncomeState } from 'src/app/state';
+import { ExpenseState, IncomeState } from 'src/app/state';
 
 @Component({
   selector: 'app-sp-summary',
@@ -13,12 +13,16 @@ export class SpSummaryComponent implements OnInit {
   monthlyIncomes$: Observable<Transaction[]>;
   incomes$: Observable<Transaction[]>;
   incomesSum: number = 0;
+  monthlyExpenses$: Observable<Transaction[]>;
+  expenses$: Observable<Transaction[]>;
+  expensesSum: number = 0;
   // TODO: Get current date from control
   currentDate: Date = new Date();
 
   constructor(public store: Store) { }
 
   ngOnInit(): void {
+    // Get incomes for the current month
     this.monthlyIncomes$ = this.store.select(
       IncomeState.selectMonthlyIncomeTransactionsForMonth(this.currentDate)
     );
@@ -30,9 +34,22 @@ export class SpSummaryComponent implements OnInit {
         this.incomesSum = monthlyIncomes
           .concat(incomes)
           .reduce((acc, cur) => acc + cur.amount, 0);
-      });
+      });  
+    });
 
-      
+    // Get expenses for the current month
+    this.monthlyExpenses$ = this.store.select(
+      ExpenseState.selectMonthlyExpenseTransactionsForMonth(this.currentDate)
+    );
+    this.expenses$ = this.store.select(
+      ExpenseState.selectTransactionsForMonth(this.currentDate)
+    );
+    this.monthlyExpenses$.subscribe((monthlyExpenses: Transaction[]) => {
+      this.expenses$.subscribe((expenses: Transaction[]) => {
+        this.expensesSum = (-1) * monthlyExpenses
+          .concat(expenses)
+          .reduce((acc, cur) => acc + cur.amount, 0);
+      });  
     });
   }
 
