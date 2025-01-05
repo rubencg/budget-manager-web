33afca3a2b1d9f23c32e862e038107b1f0422d35
Ruben Cardenas <rubencg88@gmail.com>
2025-01-05 12:32:22 -0500

    Refactored income and expenses out of the sp-summary component

--- src/app/components/spending-plan/spending-plan-content/spending-plan-content-component.ts
+++ src/app/components/spending-plan/spending-plan-content/spending-plan-content-component.ts
@@ -1,26 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { Transaction } from 'src/app/models';
+import { IncomeState, ExpenseState } from 'src/app/state';
 
 @Component({
   selector: 'app-spending-plan-content',
   templateUrl: './spending-plan-content-component.html',
   styleUrls: ['./spending-plan-content-component.scss'],
 })
-export class SpendingPlanContentComponent {
-  plannedIncome: number = 0;
-  plannedExpense: number = 0;
+export class SpendingPlanContentComponent implements OnInit {
   spentPlannedExpense: number = 0;
+
   selectedSection: string = 'income'; // Variable para controlar el contenido
+  // Income
+  monthlyIncomes$: Observable<Transaction[]>;
+  incomes$: Observable<Transaction[]>;
+  allIncomes: Transaction[];
+  incomesSum: number = 0;
+  // Expenses
+  monthlyExpenses$: Observable<Transaction[]>;
+  expenses$: Observable<Transaction[]>;
+  allExpenses: Transaction[];
+  expensesSum: number = 0;  
+  // TODO: Get current date from control
+  currentDate: Date = new Date();
+
+  constructor(public store: Store) {
+    
+  }
+  ngOnInit(): void {
+    // Get incomes for the current month
+    this.monthlyIncomes$ = this.store.select(
+      IncomeState.selectMonthlyIncomeTransactionsForMonth(this.currentDate)
+    );
+    this.incomes$ = this.store.select(
+      IncomeState.selectTransactionsForMonth(this.currentDate)
+    );
+    this.incomes$.subscribe((incomes: Transaction[]) => {
+      this.monthlyIncomes$
+        .pipe(delay(0))
+        .subscribe((monthlyIncomes: Transaction[]) => {
+          this.allIncomes = monthlyIncomes.concat(incomes);
+          this.incomesSum = this.allIncomes.reduce(
+            (acc, cur) => acc + cur.amount,
+            0
+          );
+        });
+    });
+
+    // Get expenses for the current month
+    this.monthlyExpenses$ = this.store.select(
+      ExpenseState.selectMonthlyExpenseTransactionsForMonth(
+        this.currentDate,
+        true
+      )
+    );
+    this.expenses$ = this.store.select(
+      ExpenseState.selectTransactionsForMonth(this.currentDate)
+    );
+    this.expenses$.subscribe((expenses: Transaction[]) => {
+      this.monthlyExpenses$
+        .pipe(delay(0))
+        .subscribe((monthlyExpenses: Transaction[]) => {
+          this.allExpenses = monthlyExpenses;
+
+          // Mark monthly expenses as applied
+          expenses
+            .filter((expense) => expense.monthlyKey)
+            .forEach((expense) => {
+              this.allExpenses
+                .filter((exp) => exp.key == expense.monthlyKey)
+                .forEach((exp) => {
+                  exp.applied = true;
+                });
+            });
+
+          this.expensesSum =
+            -1 * monthlyExpenses.reduce((acc, cur) => acc + cur.amount, 0);
+        });
+    });
+  }
 
   selectSection(section: string) {
     this.selectedSection = section; // Cambia el contenido mostrado
-  }
-
-  updatePlannedIncomesSum(newPlannedIncome: number): void {
-    this.plannedIncome = newPlannedIncome;
-  }
-
-  updatePlannedExpensesSum(newPlannedExpense: number): void {
-    this.plannedExpense = newPlannedExpense;
   }
   
   updateSpendPlannedExpensesSum(newPlannedExpense: number): void {
