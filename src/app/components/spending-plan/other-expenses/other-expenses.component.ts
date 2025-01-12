fec89e083a3464f74b389cf2ef26bed28a79d034
Ruben Cardenas <rubencg88@gmail.com>
2025-01-12 00:35:34 -0500

    Add pie graph to other expenses

--- src/app/components/spending-plan/other-expenses/other-expenses.component.ts
+++ src/app/components/spending-plan/other-expenses/other-expenses.component.ts
@@ -3,12 +3,15 @@ import {
   Component,
   Input,
   OnChanges,
-  OnInit,
   SimpleChanges,
 } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { DeviceDetectorService } from 'ngx-device-detector';
-import { Transaction } from 'src/app/models';
+import * as _ from 'lodash';
+import { Expense } from 'src/app/expense';
+import { PieElement, Transaction } from 'src/app/models';
+import { ChartType, ChartOptions } from 'chart.js';
+import { Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-other-expenses',
@@ -20,17 +23,30 @@ export class OtherExpensesComponent implements AfterViewInit, OnChanges {
   expensesSource = new MatTableDataSource<Transaction>();
   displayedColumns: string[];
 
+  /* Chart start */
+
+  public doughnutChartLabels: String[] = [];
+  public doughnutChartData = [];
+  public doughnutChartType: ChartType = 'pie';
+  public barChartColors: Color[] = [
+    {
+      backgroundColor: [],
+    },
+  ];
+  public options: ChartOptions = {
+    circumference: Math.PI * 2,
+    rotation: Math.PI,
+    legend: {
+      display: true,
+    },
+  };
+
+  /* Chart ends */
+
   constructor(private deviceService: DeviceDetectorService) {
     this.displayedColumns = this.deviceService.isMobile()
       ? ['transaction-content']
-      : [
-          'date',
-          'category',
-          'account',
-          'amount',
-          'notes',
-          'actions',
-        ];
+      : ['date', 'category', 'account', 'amount', 'notes', 'actions'];
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -46,5 +62,50 @@ export class OtherExpensesComponent implements AfterViewInit, OnChanges {
 
   setExpenseSource() {
     this.expensesSource = new MatTableDataSource<Transaction>(this.expenses);
+    this.setPieData();
+  }
+
+  setPieData() {
+    const appliedExpenses = this.expenses as unknown as Expense[];
+
+    // group expenses by category
+    const expensesByCategory = _.groupBy(
+      appliedExpenses,
+      (e: Expense) => e.category.name
+    );
+    const totalAmount = appliedExpenses.reduce(
+      (a, b: Expense) => +a + b.amount,
+      0
+    );
+
+    const pieElements: PieElement[] = _.map(
+      expensesByCategory,
+      function (value: Expense[], key) {
+        const categoryAmount = value.reduce(
+          (a, b: Expense) => +a + b.amount,
+          0
+        );
+        const pieElement: PieElement = {
+          description: key,
+          amount: parseFloat(categoryAmount.toFixed(2)),
+          color: value[0].category.color,
+          icon: value[0].category.image,
+          percentage:
+            parseFloat((categoryAmount / totalAmount).toFixed(2)) * 100,
+        };
+        return pieElement;
+      }
+    );
+
+    let colors: string[] = [];
+    this.doughnutChartLabels = [];
+    this.doughnutChartData = [];
+
+    pieElements.forEach((pieElement: PieElement) => {
+      this.doughnutChartLabels.push(pieElement.description);
+      this.doughnutChartData.push(pieElement.amount);
+      colors.push(pieElement.color);
+    });
+    this.barChartColors[0].backgroundColor = colors;
   }
 }
