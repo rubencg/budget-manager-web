65238aef48250f29226a87b41e507a9821bf6c22
Ruben Cardenas <rubencg88@gmail.com>
2025-01-14 22:08:20 -0500

    Added table

--- src/app/components/spending-plan/savings-component/savings-component.component.ts
+++ src/app/components/spending-plan/savings-component/savings-component.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Saving } from 'src/app/saving';
 
 @Component({
@@ -6,10 +9,28 @@ import { Saving } from 'src/app/saving';
   templateUrl: './savings-component.component.html',
   styleUrls: ['./savings-component.component.scss'],
 })
-export class SavingsComponentComponent implements OnInit {
+export class SavingsComponentComponent implements AfterViewInit, OnChanges {
   @Input() savings: Saving[];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource = new MatTableDataSource<Saving>([]);
+  displayedColumns: string[] = ['name', 'amountPerMonth', 'savedAmount', 'goalAmount', 'leftAmount'];
+
+  color: string = '#da60e0'
 
   constructor() {}
+  ngAfterViewInit(): void {
+    this.setDataSource();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['savings'] && changes['savings'].currentValue){
+      this.setDataSource()
+    }
+  }
 
-  ngOnInit(): void {}
+  setDataSource(): void {
+    this.dataSource = new MatTableDataSource<Saving>(this.savings);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
 }
