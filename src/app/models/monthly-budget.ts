3d9066c75a4a93ba4c68c7c1b9781a64e19f9dba
Ruben Cardenas <rubencg88@gmail.com>
2025-01-15 23:48:20 -0500

    Added savings sum to monthly budget dashbord

--- src/app/models/monthly-budget.ts
+++ src/app/models/monthly-budget.ts
@@ -2,6 +2,7 @@ export interface MonthlyBudget{
     incomesAmount: number,
     expensesAmount: number,
     plannedExpensesAmount: number,
+    savingsAmount: number,
     unpaidIncomesAmount: number,
     unpaidMonthlyIncomesAmount: number,
     currentBalance: number
